9c5cc237ac329650f92a51a340606461a0ff897f
bsm8734 <bsm8734@naver.com>
2020-01-03 23:53:09 +0900

    dfs with additional condition

--- SW_ExpertAcademy/D4/P_1249.cpp
+++ SW_ExpertAcademy/D4/P_1249.cpp
@@ -6,24 +6,27 @@
 using namespace std;
 
 int map[100][100];
-int max = 0;
-int min = 100000;
+int max_num = 0;
+int min_num = 100000;
 bool visit[100][100];
 
-void dfs(int cost, int x, int y) {
+void dfs(int cost, int x, int y, int depth) {
 	visit[x][y] = true;
-	cout << x << " " << y << endl;
 	cost += map[x][y];
 
-	if (x == max - 1 && y == max - 1) {
-		if (cost < min) min = cost;
+	if (cost <= min_num && depth < max_num * 2) {
+		if (x == max_num - 1 && y == max_num - 1) {
+			if (cost < min_num) min_num = cost;
+			//cout << cost << endl;
+		}
+		else {
+			if (y - 1 >= 0) if (visit[x][y - 1] == false) dfs(cost, x, y - 1, depth + 1);
+			if (x - 1 >= 0) if (visit[x - 1][y] == false) dfs(cost, x - 1, y, depth + 1);
+			if (x + 1 < max_num) if (visit[x + 1][y] == false) dfs(cost, x + 1, y, depth + 1);
+			if (y + 1 < max_num) if (visit[x][y + 1] == false) dfs(cost, x, y + 1, depth + 1);
+		}
 	}
-	else {
-		if (y - 1 >= 0 && visit[x][y - 1] == false) dfs(cost, x, y - 1);
-		if (x - 1 >= 0 && visit[x - 1][y] == false) dfs(cost, x - 1, y);
-		if (x + 1 < max && visit[x + 1][y] == false) dfs(cost, x + 1, y);
-		if (y + 1 < max && visit[x][y + 1] == false) dfs(cost, x, y + 1);
-	}
+
 
 	visit[x][y] = false;
 }
@@ -35,21 +38,21 @@ int main() {
 	cin >> T;
 	for (int t = 1; t <= T; t++) {
 		// initialization
-		min = 100000;
-		cin >> max;
+		min_num = 100000;
+		cin >> max_num;
 		char c;
-		for (int i = 0; i < max; i++) {
-			for (int j = 0; j < max; j++) {
+		for (int i = 0; i < max_num; i++) {
+			for (int j = 0; j < max_num; j++) {
 				cin >> c;
 				map[i][j] = atoi(&c);
 				visit[i][j] = false;
 			}
 		}
-		
+
 		// routine search
-		dfs(0, 0, 0);
-		cout << "#" << t << " " << min << endl;
-		
+		dfs(0, 0, 0, 0);
+		cout << "#" << t << " " << min_num << endl;
+
 	}
 	return 0;
 }
